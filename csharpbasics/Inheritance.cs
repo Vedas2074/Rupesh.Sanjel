b3819b70df377ed00849a311b854d5da32eccaba
Rupesh Sanjel <sanjelrupesh2@gmail.com>
2020-10-12 10:08:40 +0545

    learned about abstract, virtual, this etc

--- csharpbasics/Inheritance.cs
+++ csharpbasics/Inheritance.cs
@@ -5,7 +5,7 @@ public abstract class Shape
     public abstract double GetArea();
     public abstract double GetPerimeter();
 
-    public void PrintDetails()
+    public virtual void PrintDetails()
     {
         Console.WriteLine("Printing details for shape...");
     }
@@ -13,19 +13,39 @@ public abstract class Shape
 
 public class Rectangle : Shape
 {
+     public Rectangle(double l, double b)
+       {
+        
+       }
     public double Length { get; set; }
     public double Breadth { get; set; }
     public override double GetArea() => Length * Breadth;
     public override double GetPerimeter() => 2 * (Length + Breadth);
+       //public new void PrintDetails()
+       public override void PrintDetails()
+
+    {
+        Console.WriteLine("Printing details for shape...");
+        Console.WriteLine($"Length: {Length}, Breadth: {Breadth}");
+        Console.WriteLine($" Area: {this.GetArea()}, Perimeter: {this.GetPerimeter()}");
+    }
 }
 
 public class Square : Rectangle
 {
-    public double Side { get; set; }
+    public Square(double s) : base(s,s)
+    {
+
+    }
+    //public double Side { get; set; }
 }
 
 public class Circle : Shape
 {
+    public Circle(double r)
+    {
+        Radius =r; 
+    }
     public double Radius { get; set; }
     public override double GetArea() => Math.PI * Radius * Radius;
     public override double GetPerimeter() => 2 * Math.PI * Radius;
--- csharpbasics/Program.cs
+++ csharpbasics/Program.cs
@@ -3,7 +3,7 @@ using System;
 
 class Program
 {
-    static void Main2342()
+    static void Main()
     {       
         var person1 = new Person("Bishnu", "Ktm");
         var person2 = new Person("Ramesh", "Bhaktapur", 23);
@@ -19,9 +19,18 @@ class Program
         var s1 = Calculator.Sum<int>(23, 34);
         var s2 = Calculator.Sum<decimal>(233443.23m, 34344656.1m);
 
-        Rectangle rectangle = new Rectangle();
-        rectangle.Length = 23.6;
-        rectangle.Breadth = 6.32;
+        Rectangle rectangle = new Rectangle(23.4,43.4);
+        //{
+            // object initializer
+             //  rectangle.Length = 23.7;
+            //rectangle.Breadth = 6.352;
+        //};
+        rectangle.Length = 23.7;
+        rectangle.Breadth = 6.352;
         var result = rectangle.GetArea();
+        rectangle.PrintDetails();
+        //Rectangle rectangle =new Rectangle(32.6,6.32); 
+        Circle c =new Circle(23.4);
+        c.PrintDetails();
     }
 }
