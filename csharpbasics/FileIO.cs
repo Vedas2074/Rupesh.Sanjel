776529b8e065377168cc9081b5ad2d4a9fb584b8
Rupesh Sanjel <sanjelrupesh2@gmail.com>
2020-10-13 10:50:07 +0545

    learn file and directory

--- csharpbasics/FileIO.cs
+++ csharpbasics/FileIO.cs
@@ -2,7 +2,7 @@ using System;
 using System.IO;
 namespace FileDirectoryHandling
 {
-    public class FileDirectoryHandling
+    public class FileIO
     {
         public void LearnFileReading()
         {
--- csharpbasics/Program.cs
+++ csharpbasics/Program.cs
@@ -5,7 +5,7 @@ using System;
 class Program
 {
     static void Main()
-    {       
+    {
         // var person3 = new Person("Rame", "Bhaktapur", 20);
 
         // var person4 = new Person();
@@ -18,7 +18,7 @@ class Program
         // var s1 = Calculator.Sum<int>(23, 34);
         // var s2 = Calculator.Sum<decimal>(233443.23m, 34344656.1m);
 
-                
+
         // Inheritance examples
         // var r = new Rectangle(23.6, 6.32);
         // var r1 = new Rectangle(278.6, 61.32);
