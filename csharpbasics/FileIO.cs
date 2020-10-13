ce1874b16474ba7e018b4841f87272101cf9ee70
Rupesh Sanjel <sanjelrupesh2@gmail.com>
2020-10-13 10:33:17 +0545

    learn file handling and directory handling

---
+++ csharpbasics/FileIO.cs
@@ -0,0 +1,44 @@
+
+}using System;
+using System.IO;
+namespace FileDirectoryHandling
+{
+    public class FileDirectoryHandling
+    {
+        public void LearnFileReading()
+        {
+            string fileContent =File.ReadAllText("Test.txt");//or give absolute path if file not present in project
+            Console.WriteLine(fileContent);
+        }
+        public void LearnFileWriting()
+        {
+            File.WriteAllText("A.txt", "I am new just created");
+        }
+        public void LearnFileInfo()
+        {
+            FileInfo fileInfo = new FileInfo("A.txt");
+            var x =fileInfo.Length;
+            var y =fileInfo.CreationTime;
+            var z =fileInfo.DirectoryName;
+
+            Console.WriteLine($" File Size:{x} bytes");
+            Console.WriteLine($" Created:{y}");
+            Console.WriteLine($" Directory:{z}");
+
+        }
+        internal void LearnDirectory()
+        {
+            Directory.CreateDirectory("ABC");
+            File.WriteAllText($"ABC", "this is my own file");
+        }
+        internal void LearnDirectoryInfo()
+        {
+            string folderpath =@"C:\Documents\Rupesh.Sanjel";
+            DirectoryInfo directory =new DirectoryInfo(folderpath);
+            var files = directory.GetFiles();
+             Console.WriteLine("File count: " + files.Length);
+        }
+
+
+    }
+}
--- csharpbasics/Program.cs
+++ csharpbasics/Program.cs
@@ -1,36 +1,38 @@
-﻿using LearningClasses;
+﻿using FileDirectoryHandling;
+using LearningClasses;
 using System;
 
 class Program
 {
     static void Main()
     {       
-        var person1 = new Person("Bishnu", "Ktm");
-        var person2 = new Person("Ramesh", "Bhaktapur", 23);
-        var person3 = new Person("Rame", "Bhaktapur", 20);
+        // var person3 = new Person("Rame", "Bhaktapur", 20);
 
-        var person4 = new Person();
+        // var person4 = new Person();
 
-        // Accessing static members
-        var x = Person.ScientificName;
-        Person.Do();
-        var y = Math.Sqrt(342);
+        // // Accessing static members
+        // var x = Person.ScientificName;
+        // Person.Do();
+        // var y = Math.Sqrt(342);
 
-        var s1 = Calculator.Sum<int>(23, 34);
-        var s2 = Calculator.Sum<decimal>(233443.23m, 34344656.1m);
+        // var s1 = Calculator.Sum<int>(23, 34);
+        // var s2 = Calculator.Sum<decimal>(233443.23m, 34344656.1m);
 
-        Rectangle rectangle = new Rectangle(23.4,43.4);
-        //{
-            // object initializer
-             //  rectangle.Length = 23.7;
-            //rectangle.Breadth = 6.352;
-        //};
-        rectangle.Length = 23.7;
-        rectangle.Breadth = 6.352;
-        var result = rectangle.GetArea();
-        rectangle.PrintDetails();
-        //Rectangle rectangle =new Rectangle(32.6,6.32); 
-        Circle c =new Circle(23.4);
-        c.PrintDetails();
+                
+        // Inheritance examples
+        // var r = new Rectangle(23.6, 6.32);
+        // var r1 = new Rectangle(278.6, 61.32);
+        // r.PrintDetails();
+        // r1.PrintDetails();
+        // r1.GetDiagonalLength();
+
+        // var s = new Square(34.5);        
+        // s.PrintDetails();        
+
+        // var c = new Circle(23.4);
+        // c.PrintDetails();
+
+        FileIO file = new FileIO();
+        file.LearnDirectoryInfo();
     }
 }
